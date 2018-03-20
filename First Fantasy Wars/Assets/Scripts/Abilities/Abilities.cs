42f9d490b659a71e254a10a6c72b56c4aa9867fc
Richard Robinson <rrobins1@mail.sfsu.edu>
2018-03-19 21:50:26 -0700

    Added more scenes, prefabs, simple base scripts.
    Tiles have textures now, base character/ability files, some prefabs for the tiles.

---
+++ First Fantasy Wars/Assets/Scripts/Abilities/Abilities.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability : MonoBehaviour 
+{
+	string abilityName;
+	ParticleSystem abilityEffect;
+	StatusEffect effect;
+	int damage;
+	int range;
+	int radius = 1;
+}
---
+++ First Fantasy Wars/Assets/Scripts/Characters/Stats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour 
+{
+	string CharacterType;
+	int health;
+	int speed;
+	int attack;
+	int defense;
+	int move;
+	Ability abilityOne;
+	Ability abilityTwo;
+	Ability abilityThree;
+}
